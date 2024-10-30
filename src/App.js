4bd6515ee3fef1c7e3b2ff49483f1c92e3014f99
sam <samuel.mariano.ibanez@gmail.com>
2024-10-29 19:01:57 -0500

    tarea

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import './App.css'
+
+const App = () => {
+  const [persona, setPersona] = useState('')
+
+  const cambiar=()=>{
+    const ruta = 'https://randomuser.me/api/'
+    fetch(ruta)
+    .then(info => info.json())
+    .then(data => setPersona(
+      <div className='persona'>
+        <div className='nombre_completo'>
+          <h3>Nombre: {data.results[0].name.first}</h3>
+          <h3>Apellido: {data.results[0].name.last}</h3>
+        </div>
+        <img src={data.results[0].picture.large} alt='imagen'/>
+      </div>
+    ))
+    
+  }
+
+  useEffect(()=>{
+    cambiar();
+  },[])
+
+  return (
+    <div className='cuerpo'>
+      {persona}
+      <button onClick={cambiar}>Cambiar</button>
+    </div>
+  )
+}
+
+export default App
